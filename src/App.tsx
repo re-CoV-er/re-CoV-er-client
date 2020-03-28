50f9aa39cbc8b3d702f833663e528d81ddb7235d
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-03-28 12:30:57 +0100

    a fix linting issues

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { hot } from "react-hot-loader/root";
 import { Router } from '@reach/router';
 import { Landing } from './pages/landing';
-import Login  from './pages/login';
+import Login from './pages/login';
 import { Header } from './components/header';
 import Profile from './pages/profile'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { usersReducer } from './redux/reducers';
 
-export const initialState = {activeUsers: ['default active user']};
+export const initialState = { activeUsers: ['default active user'] };
 
 const App = () => {
   return (
     <Provider store={store}>
       <Header />
       <Router>
-        <Landing path="/"/>
-        <Login path="/login"/>
+        <Landing path="/" />
+        <Login path="/login" />
         <Profile path="/profile" />
       </Router>
     </Provider>
--- src/redux/reducers.ts
+++ src/redux/reducers.ts
@@ -1,11 +1,10 @@
-import { initialState } from '../App';
-import { LOG_IN } from './actions';
-
+import { initialState } from "../App";
+import { LOG_IN } from "./actions";
 
 export const usersReducer = (state = initialState, action: any) => {
-    if (action.type === LOG_IN) {
-        console.log('Payload of LOG_IN action', action.payload);
-        return { activeUsers: [...state.activeUsers, action.payload.username] }
-    }
-    return state;
+  if (action.type === LOG_IN) {
+    console.log("Payload of LOG_IN action", action.payload);
+    return { activeUsers: [...state.activeUsers, action.payload.username] };
+  }
+  return state;
 };
