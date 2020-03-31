ddde901e096bd06f3925e1f907fba568c9245b09
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-03-31 18:42:02 +0200

    F persist auth token between refreshes

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
 import { createEpicMiddleware } from "redux-observable";
 import { composeWithDevTools } from "redux-devtools-extension";
+import throttle from "lodash.throttle";
 
 import { reducer } from "./reducers";
 import { authenticationEpic } from "./epics/authentication";
+import persist from "../utils/persist";
 
 const epicMiddleware = createEpicMiddleware();
 
@@ -12,11 +14,21 @@ export function configureStore() {
     applyMiddleware(epicMiddleware)
   );
 
-  const store = createStore(reducer, composedEnhancers);
+  const store = createStore(reducer, persist.loadState(), composedEnhancers);
 
   epicMiddleware.run(authenticationEpic);
 
   return store;
 }
 
-export default configureStore();
+const store = configureStore();
+store.subscribe(
+  throttle(() => {
+    console.log("change");
+    persist.saveState({
+      authentication: store.getState().authentication,
+    });
+  }, 1000)
+);
+
+export default store;
---
+++ src/utils/persist.ts
@@ -0,0 +1,30 @@
+import { AuthorizationState } from "../redux/reducers/authentication";
+
+export class PersistState<State = { authentication: AuthorizationState }> {
+  constructor(private readonly strategy: LocalStrategy) {}
+  public loadState = () => {
+    try {
+      const serializedState = this.strategy.get("state");
+      if (serializedState == null) return undefined;
+      return JSON.parse(serializedState);
+    } catch (error) {
+      return undefined;
+    }
+  };
+
+  public saveState = (state: State) => {
+    try {
+      const serializedState = JSON.stringify(state);
+      this.strategy.set("state", serializedState);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
+
+class LocalStrategy {
+  public get = window.localStorage.getItem.bind(window.localStorage);
+  public set = window.localStorage.setItem.bind(window.localStorage);
+}
+
+export default new PersistState(new LocalStrategy());
