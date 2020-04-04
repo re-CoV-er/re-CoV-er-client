4c35b0765e426ad31b5fbbf939f0eb63d0617af5
Anna Wyss <45759648+anwyss@users.noreply.github.com>
2020-04-04 18:37:16 +0200

    update authenticationReducer with cases for LOG_IN, LOG_IN_SUCCESS and LOG_IN_FAILURE

--- src/redux/reducers/authentication.ts
+++ src/redux/reducers/authentication.ts
@@ -1,4 +1,4 @@
-import { SIGN_UP, SIGN_UP_SUCCESS, SIGN_UP_FAILURE } from "../constants";
+import { SIGN_UP, SIGN_UP_SUCCESS, SIGN_UP_FAILURE, LOG_IN, LOG_IN_FAILURE, LOG_IN_SUCCESS } from "../constants";
 import { AnyAction } from "redux";
 
 interface UserDetails {
@@ -37,6 +37,17 @@ export const authentication = (
       return {
         loading: false,
         loggedIn: false,
+      };
+    case LOG_IN:
+      console.log('LOADING')
+      return { ...state, loading: true };
+    case LOG_IN_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loggedIn: true,
+        accessToken: action.payload.accessToken,
+        userDetails: action.payload.userDetails,
       };
     default:
       return { ...state };
