5b74bc4c78c077e6248fd0760be6cfe6f431bc1c
Anna Wyss <45759648+anwyss@users.noreply.github.com>
2020-04-04 18:16:17 +0200

    f add loginEpic

---
+++ src/redux/epics/login.ts
@@ -0,0 +1,43 @@
+import { map, mergeMap, catchError, delay } from "rxjs/operators";
+import { Observable, from } from "rxjs";
+import { LogInAction } from "../action-creators/authentication";
+import { ofType, Epic } from "redux-observable";
+import { LOG_IN, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "../constants";
+import client from "../../graphql/client";
+import { logIn } from "../../graphql/authentication";
+
+export const loginEpic: Epic = (action: Observable<LogInAction>) => {
+  return action.pipe(
+    ofType(LOG_IN),
+    mergeMap((currentAction: LogInAction) => {
+      return from(
+        client.query<any>({
+          query: logIn,
+          variables: {
+            logInInput: {
+              password: currentAction.payload.password,
+              username: currentAction.payload.username,
+            },
+          },
+        })
+      ).pipe(
+        map(response => {
+          return {
+            type: LOG_IN_SUCCESS,
+            payload: {
+              accessToken: response.data?.logIn.accessToken,
+              loading: false,
+              loggedIn: true,
+            },
+          };
+        }),
+        delay(500)
+      );
+    }),
+    catchError(_error => {
+      return Promise.resolve({
+        type: LOG_IN_FAILURE,
+      });
+    })
+  );
+};
