6203b081e7982d81c16808745fb4666717201ef5
zoliv <zoliv@users.noreply.github.com>
2020-04-07 21:28:51 +0200

    f add buttons and testing fonts not compiling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,23 +2,28 @@ import styled from 'styled-components';
 import React from 'react';
 import { Link } from '@reach/router';
 
-const Heading = styled.h1`
-  display: flex;
-  justify-content: center;
-  color: lightgrey;
-  background-color:seagreen;
-  border: 2px solid black;
-  cursor: pointer;
-  `
-
 const StyledLink = styled(Link)`
   text-decoration: none;
-`
-
+  `
+const Heading = styled.h1`
+  display: flex;
+  text-align: left;
+  color: #274753;
+  background-color: white;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+  border: 2px solid black;
+  cursor: pointer;
+  display: inline;
+  font-size: 3em;
+  font-family: 'Viminalis';
+  `
 export const Header = () => {
     return (
+      <span>
         <StyledLink to="/">
-            <Heading>Re CoV eR</Heading>
+            <Heading>re-CoV-er </Heading>
         </StyledLink>
+      </span>  
     )
 }
