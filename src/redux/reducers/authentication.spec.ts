ab548bfabfb67976d17b31d4308479dbf7a1eb0c
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-04-09 17:15:26 +0200

    r fix linting issue and make code more explicit

--- src/redux/reducers/authentication.spec.ts
+++ src/redux/reducers/authentication.spec.ts
@@ -2,11 +2,9 @@ import { authentication } from "./authentication";
 
 describe("authentication", () => {
   it("should", () => {
-    let state;
-    state = authentication(
-      { loading: true, loggedIn: false },
-      { type: "LOG_IN_FAILURE" }
-    );
+    const previousState = { loading: true, loggedIn: false };
+    const action = { type: "LOG_IN_FAILURE" };
+    const state = authentication(previousState, action);
     expect(state).toEqual({
       loading: false,
       loggedIn: false,
