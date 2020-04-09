3c0aed8370e585fcd840458c064a103d071f652a
guillem <44653501+guillem-gelabert@users.noreply.github.com>
2020-04-09 17:31:55 +0200

    e setup prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,16 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     extends: [
         "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
     ],
     parserOptions: {
         ecmaFeatures: {
             jsx: true
-        }
+        },
+        ecmaVersion: "ESNEXT",
+        sourceType: "module"
     },
     settings: {
         react: {
